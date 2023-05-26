f12c635a2ff7e327d3dc5b8fcd353f95651b7c74
marcushoang888 <marcushoang.888@gmail.com>
2023-05-26 16:27:28 +0700

    files

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
+import express from "express";
+import cors from "cors";
+import { Sequelize } from "sequelize";
 
-import express from "express"
-import cors from "cors"
+const app = express();
+const port = process.env.PORT;
+app.use(express());
+app.use(cors());
 
-const app = express()
-const port = 3000
-app.use(express())
-app.use(cors())
+const sequelize = new Sequelize("test_bj2r", "test", "DbPCDoVPAy1G2rKI7KqkmDUpjZcO5QSC", {
+  host: "postgres://test:DbPCDoVPAy1G2rKI7KqkmDUpjZcO5QSC@dpg-chnq99o2qv207f2q8fog-a/test_bj2r",
+  dialect: "postgres",
+});
 
-app.listen(port, () => {
-    console.log("Server is running");
-})
+async function testing ()  {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+}
+
+testing()
+
+app.listen(port || 3000, () => {
+  console.log("Server is running");
+});
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "success",
+  });
+});
