53a032809ae9f37e52ea0e3ec5cf2c0fe190b4c9
marcushoang888 <marcushoang.888@gmail.com>
2023-05-26 16:34:36 +0700

    files

--- index.js
+++ index.js
@@ -7,28 +7,34 @@ const port = process.env.PORT;
 app.use(express());
 app.use(cors());
 
-const sequelize = new Sequelize("test_bj2r", "test", "DbPCDoVPAy1G2rKI7KqkmDUpjZcO5QSC", {
-  host: "postgres://test:DbPCDoVPAy1G2rKI7KqkmDUpjZcO5QSC@dpg-chnq99o2qv207f2q8fog-a/test_bj2r",
-  dialect: "postgres",
-});
+const sequelize = new Sequelize(
+  "test_bj2r",
+  "test",
+  "DbPCDoVPAy1G2rKI7KqkmDUpjZcO5QSC",
+  {
+    host: "postgres://test:DbPCDoVPAy1G2rKI7KqkmDUpjZcO5QSC@dpg-chnq99o2qv207f2q8fog-a.oregon-postgres.render.com/test_bj2r",
+    dialect: "postgres",
+  }
+);
 
-async function testing ()  {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }
+async function testing() {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 }
 
-testing()
+testing();
 
 app.listen(port || 3000, () => {
   console.log("Server is running");
 });
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const jane = await User.create({ firstName: "Jane", lastName: "Doe" });
   res.json({
-    message: "success",
+    message:  jane.id,
   });
 });
